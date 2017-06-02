5fd10f10296edd54fdfc4f23705d7b16d5c93dc4
Deepak <deepak@deepakmac.local>
2017-06-02 15:58:44 +0530

    Initial Commit

---
+++ mlframework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mlframework
+//
+//  Created by Deepak on 02/06/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
