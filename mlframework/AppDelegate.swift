a1667cd19395923e10f524fc63fcf1d82b8e9dff
Deepak <deepak@deepakmac.local>
2017-06-02 16:07:52 +0530

    Initial commit

--- mlframework/AppDelegate.swift
+++ mlframework/AppDelegate.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 import CoreData
+//import ml
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+   // var ref = ml()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //print("console",ref.setup(isDebug: true))
         return true
     }
 
